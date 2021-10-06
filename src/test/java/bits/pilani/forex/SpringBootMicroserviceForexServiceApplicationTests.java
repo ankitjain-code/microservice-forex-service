acc6e9adc904458e37359f98a196ea2f1144a77d
ankit <ankitjain19962013@gmail.com>
2021-10-06 19:36:20 +0530

    foreign exchange micro-service

---
+++ src/test/java/bits/pilani/forex/SpringBootMicroserviceForexServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package bits.pilani.forex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMicroserviceForexServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
